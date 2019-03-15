065e19235f930de982089d6c636f451b440ce222
anotheruser191115 <43791330+anotheruser191115@users.noreply.github.com>
2019-03-15 16:21:02 -0300

    Update ejercicio-02.py

--- practico_01/ejercicio-02.py
+++ practico_01/ejercicio-02.py
@@ -2,4 +2,14 @@
 
 
 def mayor(a, b, c):
-    pass
+    if a>=b:
+        if a>=c:
+            return a
+        return c
+    elif b>=c:
+        return b
+    return c
+
+assert mayor(5,10,6) == 10
+assert mayor(10,5,6) == 10
+assert mayor(5,6,10) == 10
